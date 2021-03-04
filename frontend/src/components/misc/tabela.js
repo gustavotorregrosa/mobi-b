8b45585155c8e1ee52b1dd245820a6c5c34b070f
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2021-03-04 17:34:34 -0300

    parcial

--- frontend/src/components/misc/tabela.js
+++ frontend/src/components/misc/tabela.js
@@ -1,15 +1,28 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer, useState } from 'react'
 
 const Tabela = props => {
 
     const { campos, data, eventName, loading } = props
     const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
 
+    const [mPaginacao, setmPaginacao] = useState({})
+    const numItensPorPag = 5
+
     useEffect(() => {
         document.addEventListener('atualiza-tabela', () => {
             forceUpdate()
         })
-    }, [])
+
+
+        let mPaginacao = {
+            min: 1,
+            max: Math.ceil(data.length / numItensPorPag),
+            current: 1
+        }
+
+        setmPaginacao(mPaginacao)
+
+    }, [data])
 
     const geraCabecalho = () => {
         let lista = campos.map(campo => (<th>{campo.label}</th>))
@@ -77,13 +90,44 @@ const Tabela = props => {
         return null
     }
 
+    const geraPaginacao = () => {
+
+        let leftChev = (<li className="disabled"><a href="#!"><i className="material-icons">chevron_left</i></a></li>)
+        if(mPaginacao.current > 1){
+            leftChev = (<li className="waves-effect"><a href="#!"><i className="material-icons">chevron_left</i></a></li>)
+        }
+
+        let rightChev = (<li className="waves-effect"><a href="#!"><i className="material-icons">chevron_right</i></a></li>)
+        if(mPaginacao.current == mPaginacao.max){
+            rightChev = (<li className="disabled"><a href="#!"><i className="material-icons">chevron_right</i></a></li>)
+        }
+
+        let itensPaginacao = []
+        for (let index = 1; index <= mPaginacao.max; ++index) {
+            let item = (<li className="waves-effect"><a href="#!">{index}</a></li>)
+            if(index == mPaginacao.current){
+                item = (<li className="active black"><a href="#!">{index}</a></li>)
+            }
+            itensPaginacao.push(item)
+        }
+
+        return (<ul className="pagination">
+            {leftChev}
+            {itensPaginacao}
+            {rightChev}    
+        </ul>
+                      
+        )
+    }
+
     const geraTabela = () => (<div>
         <table className="highlight responsive-table">
             {geraCabecalho()}
             {geraItens()}
-            
+               
         </table>
         {loader()}
+        {geraPaginacao()}
     </div>
     )
 
