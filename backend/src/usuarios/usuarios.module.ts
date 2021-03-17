20ddc35d60268c247dc3b14d864b1e0b89a1a3ee
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2021-03-17 18:58:31 -0300

    parcial

--- backend/src/usuarios/usuarios.module.ts
+++ backend/src/usuarios/usuarios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsuarioSchema } from './interfaces/usuario.schema';
 import { UsuariosController } from './usuarios.controller';
 import { UsuariosService } from './usuarios.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Usuario', schema: UsuarioSchema}])],
   controllers: [UsuariosController],
   providers: [UsuariosService], //UsuariosService]
 })
